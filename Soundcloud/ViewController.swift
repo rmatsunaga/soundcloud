2f760d57d6b62ecc3ea7f1270bd82a45d3528c26
Rey Matsunaga <matsunagar97@gmail.com>
2019-01-29 16:27:52 -0800

    first commit

---
+++ Soundcloud/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Soundcloud
+//
+//  Created by Rey Matsunaga on 1/29/19.
+//  Copyright © 2019 Rey Matsunaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
